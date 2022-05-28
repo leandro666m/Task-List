c96988b0df0893528088ad3c9275cb55015f68d6
leandro666m <leandro666m@gmail.com>
2022-05-28 20:09:18 -0300

    mock

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar el 1er modulo de Angular",
+        day: "Lunes 13 12",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Terminar Bootstrap",
+        day: "Lunes 13 12",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Ejercitar 1er modulo de Angular",
+        day: "Lunes 13 12",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Hacer mate",
+        day: "Lunes 13 12",
+        reminder: false
+    } ,
+
+]
